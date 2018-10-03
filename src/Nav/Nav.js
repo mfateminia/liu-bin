945635c562df2f3b681e7dac9a50bcdb5b57a739
Seyed Mohammad Ali Fateminia <mfateminia@gmail.com>
2018-10-03 20:38:40 +0200

    nav progress

--- src/Nav/Nav.js
+++ src/Nav/Nav.js
@@ -1,43 +1,46 @@
 import React from 'react';
 import './Nav.css';
+import logo from '../img/logo.png';
 
-const Nav = () => {
+class Nav extends React.Component {
+    state ={
+        classNames: '-nav-inner-wrapper'
+    };
 
-    const navItems = [
-        {
-            text: 'Home',
-            url: './index.html'
-        },
-        {
-            text: 'Research',
-            url: './research.html'
-        },
-        {
-            text: 'Publications',
-            url: './publications.html'
-        },
-        {
-            text: 'People',
-            url: './people.html'
-        },
-        {
-            text: 'Contact Us',
-            url: './contact.html'
+    toggle = () => {
+        if (this.state.classNames === '-nav-inner-wrapper') {
+            this.setState({classNames: '-nav-inner-wrapper -nav-toggler'});
+        } else {
+            this.setState({classNames: '-nav-inner-wrapper'});
         }
-    ];
-
-    const assembleNav = (items) => {
-        return (
-            <ul className = 'nav-items'>{
-                items.map((item, index) => <li className = 'nav-item'>{item.text}</li>)
-                }
-            </ul>);
-
     }
 
-    return (
-        <div className = 'nav-container'>{assembleNav(navItems)}</div>
-    );
+    render(){
+        return(
+            <div className='-nav-wrapper'>
+            <div className = '-nav-brand'>
+                <img src = {logo}/>
+            </div>
+
+            <a href="javascript:void(0);" className = '-nav-icon' onClick={this.toggle}>
+                <i className = "fa fa-bars"></i>
+            </a>
+            <div className = {this.state.classNames}>
+
+                    <a href="#home" className = '-nav-link'>HOME</a>
+                    <a href="#news" className = '-nav-link'>PUBLICATIONS</a>
+                    <a href="#contact" className = '-nav-link'>PEOPLE</a>
+                    <a href="#about" className = '-nav-link'>CONTACT</a>
+
+            </div>
+
+        </div>
+        );
+    }
+
+
+
+
 }
 
 export default Nav;
