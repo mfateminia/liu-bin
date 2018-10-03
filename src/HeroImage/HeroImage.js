229450d1d74bdb614b1cd0e10fbdb4a8d3cd4d8b
Seyed Mohammad Ali Fateminia <mfateminia@gmail.com>
2018-10-03 08:30:52 +0200

    HeroImage created

---
+++ src/HeroImage/HeroImage.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './HeroImage.css';
+import heroImage from '../img/hero_image.jpg';
+
+const HeroImage = () =>{
+    return (
+        <div className = '-hero-image-outer-wrapper'>
+            <div className = '-hero-image-inner-wrapper'>
+                <div className = '-hero-image-image-wrapper bg-primary'>
+                        <img className = '-hero-image-image' src = {heroImage} />
+                        <div className = '-hero-image-overlay'></div>
+                </div>
+
+                <div className ="-hero-image-text-wrapper text-white text-center container-fluid">
+                    <h1>Liu's Research Group</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                    <div className = '-hero-image-button'>
+                        Read more
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default HeroImage;
