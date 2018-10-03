9f81838d0c1d4e4f954bf244c39c6f95d7816339
Seyed Mohammad Ali Fateminia <mfateminia@gmail.com>
2018-10-03 20:41:05 +0200

    progress

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import Nav from './Nav/Nav';
+import HeroImage from './HeroImage/HeroImage';
 import HorizCard from './Card/HorizCard';
 import Footer from './Footer/Footer';
-// import { library } from '@fortawesome/fontawesome-svg-core'
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-// import { faStroopwafel } from '@fortawesome/free-solid-svg-icons'
+import News from './News/News';
+import Research from './Research/Research';
+import People from './People/People';
 import './App.css';
 
 // library.add(faStroopwafel);
@@ -12,8 +13,14 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App container-fluid text-primary">
+      <div className="-app-wrapper container-fluid text-primary">
         <Nav />
+        <HeroImage />
+        <People />
+        <News />
+        <h1 className = '-app-titles container'>Research</h1>
+        <Research />
+        <h1 className = '-app-titles container'>Recent Publications</h1>
         <HorizCard imgUrl = 'https://cdn.stocksnap.io/img-thumbs/960w/DGWQ8ZWZQS.jpg'
           title = 'A Light-Up Probe with Aggregation-Induced Emission for Real-Time Bio-orthogonal Tumor Labelling and Image-Guided Photodynamic Therapy'
           subtitle = 'Guangxue Feng and Bin Liu'
