379a67d756101aa2355b9554d9f462a3edeb6848
Seyed Mohammad Ali Fateminia <mfateminia@gmail.com>
2018-10-03 22:00:48 +0200

    Reseachers created

---
+++ src/People/Researchers/Researchers.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Researchers.css';
+
+const Researchers = () => {
+    return (
+        <img src = {process.env.PUBLIC_URL + './img/bin_liu.jpg'} />
+    );
+}
+
+export default Researchers;
