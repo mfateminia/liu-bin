f008d851cbee63135e2d614aa68bea5e1acb5ccc
Seyed Mohammad Ali Fateminia <mfateminia@gmail.com>
2018-10-03 20:38:21 +0200

    footer progress

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -4,31 +4,33 @@ import './Footer.css';
 
 const Footer = () => {
 	return (
-		<div className = '-outer-wrapper container-fluid bg-primary text-light'>
+		<div className = '-footer-outer-wrapper container-fluid bg-primary text-light'>
             
-            <div className = '-inner-wrapper container'>
+            <div className = '-footer-inner-wrapper container'>
                 
                 <div className = 'row'>
                     <div className = 'col-11 row'>
-                        <div className = '-address col-md-7 order-2 order-md-1'>                        
+                        <div className = '-footer-address col-md-7 order-2 order-md-1'>                        
                             Address: National University of Singapore
                             Department of Chemical and Biomolecular Engineering, 
                             4 Engineering Drive 4, Blk E5 #02-17,
                             Singapore 117585 
-                            <p className = 'view-on-map'> <i className="fas fa-map-marker-alt -icon"></i>view on map</p>						
+                            <p className = 'view-on-map'> 
+                            <i className="-footer-icon fas fa-map-marker-alt">
+                            </i>view on map</p>						
                         </div>	
-                        <div className = '-contact col-md-5 order-1 order-md-2'>
+                        <div className = '-footer-contact col-md-5 order-1 order-md-2'>
 
-                            <div className = '-email'>
-                            <i className="fas fa-envelope -icon"></i>
+                            <div className = '-footer-email'>
+                            <i className="-footer-icon fas fa-envelope"></i>
                                 cheliub@nus.edu.sg
                             </div>
-                            <div className = '-telephone'>
-                            <i className="fas fa-phone -icon"></i>
+                            <div className = '-footer-telephone'>
+                            <i className="-footer-icon fas fa-phone"></i>
                                 +65 6516 8049
                             </div>	
-                            <div className = '-fax'>
-                            <i className="fas fa-fax -icon"></i>
+                            <div className = '-footer-fax'>
+                            <i className="-footer-icon fas fa-fax -icon"></i>
                                 +65 6779 1936
                             </div>						
                         </div>						
