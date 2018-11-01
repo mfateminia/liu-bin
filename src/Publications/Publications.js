a617ec99610a4d3f83f8bc11c86ef58f8bbf0532
Ali <mfateminia@gmail.com>
2018-11-01 23:22:51 +0100

    functions structures improved

--- src/Publications/Publications.js
+++ src/Publications/Publications.js
@@ -9,12 +9,12 @@ class FilteredPublications extends React.Component {
         return (
             <div className = '-publications-search-box-wrapper'>
                 <form id = '-publications-search-form-id' className = '-publications-search-box'>
-                    <input id = '-publications-search-input-id' className = '-publications-search-input' type="text" placeholder = 'Search in publications' onChange = {this.filterThem}></input>
-                    <select id = '-publications-search-year-id' className = '-publications-search-dropdown' onChange = {this.filterThem}>
-                        {this.buildYearsFilter()}
+                    <input id = '-publications-search-input-id' className = '-publications-search-input' type="text" placeholder = 'Search in publications' onChange = {this.applyFilter}></input>
+                    <select id = '-publications-search-year-id' className = '-publications-search-dropdown' onChange = {this.applyFilter}>
+                        {this.buildDropdown('year')}
                     </select>
-                    <select id = '-publications-search-type-id' className = '-publications-search-dropdown' onChange = {this.filterThem}>
-                        {this.buildTypesFilter()}
+                    <select id = '-publications-search-type-id' className = '-publications-search-dropdown' onChange = {this.applyFilter}>
+                        {this.buildDropdown('type')}
                     </select>
                     <button className = '-publications-search-button' onClick = {this.clearFilters}>Clear</button>
                 </form>
@@ -22,55 +22,44 @@ class FilteredPublications extends React.Component {
         );
     }
 
-    buildYearsFilter(){
-        let yearsArr = PublicationsAPI.map(item => item.year);
-        yearsArr = this.uniqueSort(yearsArr);
-
-        yearsArr.unshift('All years');
-        return yearsArr.map((item, index) => <option key = {index}>{item}</option>);        
+    buildDropdown(FilterField){
+        let arr = PublicationsAPI.map(item => item[FilterField]);
+        arr = this.uniqueAndSort(arr);
+        FilterField === 'year' ? arr.unshift('All years') : FilterField;
+        FilterField === 'type' ? arr.unshift('All Types') : FilterField;
+        return arr.map((item, index) => <option key = {index}>{item}</option>);        
     }
 
-    buildTypesFilter(){
-        let typesArr = PublicationsAPI.map(item => item.type);
-        typesArr = this.uniqueSort(typesArr);
-
-        typesArr.unshift('All Types');
-        return typesArr.map((item, index) => <option key = {index}>{item}</option>);
+    uniqueAndSort(arr){
+        if (arr.length === 0) return null;        
+        arr = this.sortArray(arr);
+        arr = this.removeDuplicates(arr);
+        return arr;
     }
 
-    uniqueSort(arr){
-        if (arr.length === 0) return null;
+    sortArray(arr){
+        arr = (isNaN(arr[0])? arr.sort() : arr.sort((a, b) => b - a));
+        return arr;
+    }
 
-        //sort array
-        isNaN(arr[0])? arr = arr.sort() : arr = arr.sort((a, b) => b - a)
-
-        //remove duplicates
-        let uniqueSorted = [arr[0]];
-        for (let i = 1; i < arr.length; i++) {
-          if (arr[i-1] !== arr[i]) {
-            uniqueSorted.push(arr[i]);
+    removeDuplicates(arr){
+        let uniqueSorted = [];
+        for (let CurrentItem of arr) {
+            PreviousItem !== CurrentItem ? uniqueSorted.push(CurrentItem) : null
+            let PreviousItem = CurrentItem; 
           }
-        }
         return uniqueSorted;
     }
 
-    filterThem = () => {
+    applyFilter = () => {
         const textFilterValue = document.getElementById('-publications-search-input-id').value;
         const yearFilterValue = document.getElementById('-publications-search-year-id').value;
         const typeFilterValue = document.getElementById('-publications-search-type-id').value;
+
         var filteredResults = [...PublicationsAPI];
-
-        if(textFilterValue){
-            filteredResults = filteredResults.filter(item => (item.title.toLowerCase().includes(textFilterValue.toLowerCase())));
-        } 
-
-        if(yearFilterValue !== 'All years'){
-            filteredResults = filteredResults.filter(item => (item.year === yearFilterValue));
-        }
-
-        if(typeFilterValue !== 'All Types'){
-            filteredResults = filteredResults.filter(item => (item.type === typeFilterValue));
-        }
+        filteredResults = (textFilterValue ? filteredResults.filter(item => (item.title.toLowerCase().includes(textFilterValue.toLowerCase()))) : filteredResults)
+        filteredResults = (yearFilterValue !== 'All years' ? filteredResults.filter(item => (item.year === yearFilterValue)) : filteredResults);
+        filteredResults = (typeFilterValue !== 'All Types' ? filteredResults.filter(item => (item.type === typeFilterValue)) : filteredResults);
 
         this.props.store.applyFilters(filteredResults);
     }
